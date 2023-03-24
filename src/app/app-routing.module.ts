11a920ea8ab80f8f008ddc1fc4ba212517a6add8
fabiannaglo <fabiannaglo@outlook.de>
2023-03-24 16:21:04 +0100

    Darkmode angepasst
    Cookie zu Darkmode hinzugefügt.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MonthOutputComponent } from './month-output/month-output.component';
 import { TimeInputComponent } from './time-input/time-input.component';
 import { WeekOutputComponent } from './week-output/week-output.component';
+import { Cookie } from 'ng2-cookies';
 
 
 const routes: Routes = [
@@ -16,5 +17,22 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {
+  constructor() {
+    this.checkDarkmode();
+  }
 
+  checkDarkmode(){
+    if(Cookie.get("darkmode") == "1"){
+      document.documentElement.classList.add("darkmode");
+      document.querySelector('meta[name="theme-color"]')!.setAttribute('content', '#3de8a4');
+    }
+    else{
+      document.documentElement.classList.remove("darkmode");
+      document.querySelector('meta[name="theme-color"]')!.setAttribute('content', '#c2185b');
+    }
+
+    window.setTimeout(function(){
+      document.documentElement.classList.add("transition");
+    },300)
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Time } from '../time';
 import { TimeService } from '../time.service';
+import { Cookie } from 'ng2-cookies';
 
 @Component({
   selector: 'app-nav',
@@ -29,7 +30,28 @@ export class NavComponent {
     window.location.reload();
   }
 
+  async refresh(){
+    this.timeInput = await this.timeService.getAll();
+  }
+
+  private setCookie(name: string, value: string, expireDays: number, path: string = '') {
+    let d:Date = new Date();
+    d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
+    let expires:string = `expires=${d.toUTCString()}`;
+    let cpath:string = path ? `; path=${path}` : '';
+    document.cookie = `${name}=${value}; ${expires}${cpath}`;
+  }
+
   changeDarkmode(){
     document.documentElement.classList.toggle("darkmode");
+    
+    if(document.documentElement.classList.contains("darkmode")){
+      Cookie.set("darkmode", "1", 365);
+      document.querySelector('meta[name="theme-color"]')!.setAttribute('content', '#3de8a4');
+    }
+    else{
+      Cookie.set("darkmode", "0", 365);
+      document.querySelector('meta[name="theme-color"]')!.setAttribute('content', '#c2185b');
+    }
   }
 }
