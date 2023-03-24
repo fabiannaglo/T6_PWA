5871f71aaa2c4a11f6dec5c88a99bf13b4806f77
fabiannaglo <fabiannaglo@outlook.de>
2023-03-24 16:16:37 +0100

    Eingaben löschen eingefügt

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Time } from '../time';
+import { TimeService } from '../time.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,6 +11,8 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  timeInput: Time[] = [];
+  deferredPrompt: any;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,12 +20,13 @@ export class NavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private timeService: TimeService, private breakpointObserver: BreakpointObserver) {
  
   }
 
   clearInputs(){
-    console.log("Eingaben löschen");
+    this.timeService.clearAll();
+    window.location.reload();
   }
 
   changeDarkmode(){
